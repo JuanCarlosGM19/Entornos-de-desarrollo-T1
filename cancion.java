75cb844ee93d223c3675623af7bef2c6f4956cde
JuanCarlosGM19 <juancarlosgarciamancheno@gmail.com>
2024-11-07 12:21:50 +0100

    Update cancion.java

--- cancion.java
+++ cancion.java
@@ -4,7 +4,7 @@ public class cancion
     public static void main(String args[])
         {
         	
-        	System.out.println("Hola Don Pepito");
+        	System.out.println("Hola, me presento, mi nombre es Kike Montilla. Me conocen por el barrio como El niño maravilla");
         	
 		}
 }
